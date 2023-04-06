20e6877bf25640ffde146799131ecdbab2fcf6a6
Max Stabel <M4a1x@users.noreply.github.com>
2023-04-06 15:40:35 +0200

    Simplify `my_first_experiment.py`

--- workspace/marcos/sequences/src/sequences/my_first_experiment.py
+++ workspace/marcos/sequences/src/sequences/my_first_experiment.py
@@ -13,11 +13,8 @@ sys.path.append(str(marcos_client))
 
 def my_first_experiment():
     exp = ex.Experiment(lo_freq=5, rx_t=3.125)
-    tx0_times = np.array([50, 130])
-    tx0_amps = np.array([0.5, 0])
 
-    event_dict = {"tx0": (tx0_times, tx0_amps)}
-    exp.add_flodict(event_dict)
+    exp.add_flodict({"tx0": (np.array([50, 130]), np.array([0.5, 0]))})
     exp.add_flodict({"rx0_en": (np.array([200, 400]), np.array([1, 0]))})
 
     exp.plot_sequence()
