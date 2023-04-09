947fbf132f59800c68965bac6ffdabf524829699
Max Stabel <M4a1x@users.noreply.github.com>
2023-04-10 00:52:32 +0200

    Use my branch of marcos_client

--- workspace/marcos/sequences/hatch_build.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-
-from git import Repo
-from hatchling.builders.hooks.plugin.interface import BuildHookInterface
-
-
-class CustomHook(BuildHookInterface):
-    def initialize(self, _version: str, _build_data: dict[str, Any]) -> None:
-        Repo.clone_from("https://github.com/vnegnev/marcos_client.git", "src/sequences/marcos_client")
--- workspace/marcos/sequences/src/sequences/my_first_experiment.py
+++ workspace/marcos/sequences/src/sequences/my_first_experiment.py
@@ -1,23 +1,16 @@
 import logging
-import sys
-from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 logger = logging.getLogger(__name__)
 
-# dirty hack, cause marcos_client uses direct imports for neighbouring files
-marcos_client_path = (Path(__file__).parent / "marcos_client").resolve()
-sys.path.append(str(marcos_client_path))
-logger.info("Added %s to python path", str(marcos_client_path))
 
-# Needs to be after sys.path hack
-from sequences.marcos_client import experiment as ex  # noqa: E402, pylint: disable=C0413
+from marcos import Experiment
 
 
 def my_first_experiment():
-    exp = ex.Experiment(lo_freq=5, rx_t=3.125)
+    exp = Experiment(lo_freq=5, rx_t=3.125)
 
     exp.add_flodict({"tx0": (np.array([50, 130]), np.array([0.5, 0]))})
     exp.add_flodict({"rx0_en": (np.array([200, 400]), np.array([1, 0]))})
@@ -31,7 +24,7 @@ def my_first_experiment():
 
 
 def my_second_experiment():
-    exp = ex.Experiment(lo_freq=5, rx_t=3.125)
+    exp = Experiment(lo_freq=5, rx_t=3.125)
 
     event_dict = {
         "tx0": (np.array([50, 130, 200, 360]), np.array([0.5, 0, 0.5j, 0])),
