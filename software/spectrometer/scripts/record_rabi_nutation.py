0cf06439817710a5cdf89cf96295a833eb2191d2
Max Stabel <M4a1x@users.noreply.github.com>
2023-08-22 13:36:09 +0200

    Add rabi nutation for thesis

--- software/spectrometer/scripts/record_rabi_nutation.py
+++ software/spectrometer/scripts/record_rabi_nutation.py
@@ -3,10 +3,11 @@
 
 import logging
 from pathlib import Path
+from datetime import UTC, datetime as dt
 
 import numpy as np
 
-from spectrometer import FID1D, NMRSequence, Server, Spectrometer
+from spectrometer import FID1D, NMRSequence, Server, Spectrometer, ConnectionSettings
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -16,9 +17,9 @@ def main() -> None:
     """Send series of pulses with increasing length"""
 
     logger.info("Creating pulse sequences...")
-    pulse_lengths_us = np.linspace(1, 180, 90)
-    delay_us = 30
-    repetition_time_s = 1
+    pulse_lengths_us = np.linspace(1, 300, 900)
+    delay_us = 25
+    repetition_time_s = 5
     sequences = [
         NMRSequence.simple(
             pulse_length_us=pulse_length_us, delay_us=delay_us, record_length_us=10e3
@@ -33,7 +34,8 @@ def main() -> None:
     server.start()
 
     logger.info("Connecting to spectrometer server and sending pulse sequences...")
-    spec = Spectrometer(tx_freq=25_090_000)
+    config = ConnectionSettings(ip_address="192.168.1.100", port=11111)
+    spec = Spectrometer(tx_freq=25_089_900, sample_rate=320e3, server_config=config)
     spec.connect()
     datas = spec.send_sequences(
         sequences=sequences, repetition_time_s=repetition_time_s
@@ -41,6 +43,7 @@ def main() -> None:
     spec.disconnect()
 
     logger.info("Saving FIDs...")
+    timestamp = dt.now(tz=UTC).replace(microsecond=0)
     for i, data in enumerate(datas):
         fid = FID1D(
             data=data,
@@ -51,6 +54,7 @@ def main() -> None:
             sample="Water",
             pulse=f"one_of_repeated_90_degree_pulses,length={pulse_lengths_us[i]}us,delay={delay_us}us,repetition_time={repetition_time_s}s",
             spectrometer="magnETHical v0.1",
+            timestamp=timestamp,
         )
         timestr = fid.timestamp.strftime("%Y%m%d-%H%M%S")
         file = (
